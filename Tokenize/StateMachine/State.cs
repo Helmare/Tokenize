b4e24b719b68452a3f6ed80e588a5fb3d1e99637
hazdryx <hazdryx@gmail.com>
2019-09-23 22:12:23 -0500

    Added "valid if in set" transitions.

--- Tokenize/StateMachine/State.cs
+++ Tokenize/StateMachine/State.cs
@@ -27,7 +27,7 @@ namespace Tokenize.StateMachine
         ///     Add a transition to the list.
         /// </summary>
         /// <param name="transition">The transition to be added.</param>
-        /// <returns>This TransitionalState for chaining.</returns>
+        /// <returns>The current State for chaining.</returns>
         public State<T> AddTransition(StateTransition<T> transition)
         {
             Transitions.Add(transition);
@@ -39,7 +39,7 @@ namespace Tokenize.StateMachine
         /// <param name="nextState">The next state if the transition is valid.</param>
         /// <param name="validator">The delegate to check if the bit is valid.</param>
         /// <param name="action">The action to perform when the transition is valid.</param>
-        /// <returns>This TransitionalState for chaining.</returns>
+        /// <returns>The current State for chaining.</returns>
         public State<T> AddTransition(int nextState, BitValidator<T> validator, StateAction<T> action)
         {
             return AddTransition(new StateTransition<T>(nextState, validator, action));
@@ -52,16 +52,37 @@ namespace Tokenize.StateMachine
         /// <param name="nextState">The next state if the transition is valid.</param>
         /// <param name="validBit"></param>
         /// <param name="action">The action to perform when the transition is valid.</param>
+        /// <returns>The current State for chaining.</returns>
         public State<T> AddTransition(int nextState, T validBit, StateAction<T> action)
         {
-            return AddTransition(new StateTransition<T>(nextState, (bit) => bit.Equals(validBit), action));
+            return AddTransition(nextState, (bit) => bit.Equals(validBit), action);
+        }
+        /// <summary>
+        ///     Adds s state transition which is only
+        ///     valid when the bit is part of the
+        ///     <code>validBits</code> set.
+        /// </summary>
+        /// <param name="nextState">The next state if the transition is valid.</param>
+        /// <param name="validBits"></param>
+        /// <param name="action">The action to perform when the transition is valid.</param>
+        /// <returns>The current State for chaining.</returns>
+        public State<T> AddTransition(int nextState, IEnumerable<T> validBits, StateAction<T> action)
+        {
+            return AddTransition(nextState, (bit) =>
+            {
+                foreach(T validBit in validBits)
+                {
+                    if (bit.Equals(validBit)) return true;
+                }
+                return false;
+            }, action);
         }
         /// <summary>
         ///     Adds a transition to the list.
         /// </summary>
         /// <param name="nextState">The nex state if the transition is valid.</param>
         /// <param name="validator">The delegate to check if the bit is valid.</param>
-        /// <returns>The TransitionState for chaining.</returns>
+        /// <returns>The current State for chaining.</returns>
         public State<T> AddTransition(int nextState, BitValidator<T> validator)
         {
             return AddTransition(nextState, validator, null);
@@ -73,9 +94,22 @@ namespace Tokenize.StateMachine
         /// </summary>
         /// <param name="nextState">The next state if the transition is valid.</param>
         /// <param name="validBit"></param>
+        /// <returns>The current State for chaining.</returns>
         public State<T> AddTransition(int nextState, T validBit)
         {
             return AddTransition(nextState, validBit, null);
+        }
+        /// <summary>
+        ///     Adds s state transition which is only
+        ///     valid when the bit is part of the
+        ///     <code>validBits</code> set.
+        /// </summary>
+        /// <param name="nextState">The next state if the transition is valid.</param>
+        /// <param name="validBits"></param>
+        /// <returns>The current State for chaining.</returns>
+        public State<T> AddTransition(int nextState, IEnumerable<T> validBits)
+        {
+            return AddTransition(nextState, validBits, null);
         }
     }
 }
